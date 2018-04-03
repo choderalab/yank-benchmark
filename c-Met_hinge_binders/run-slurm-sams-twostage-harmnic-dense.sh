ed09917c08da3d9c6d1e1934b0860a76a035c0b4
Levi Naden <levi.naden@choderalab.org>
2018-04-03 10:25:20 -0400

    Add SLURM input files to the branch

---
+++ c-Met_hinge_binders/run-slurm-sams-twostage-harmnic-dense.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#Walltime
+#SBATCH -t 72:00:00
+#
+#SBATCH -o "slurm-%j.out"
+#SBATCH -e "slurm-%j.err"
+#
+#partition
+#SBATCH --partition=GTX
+#
+# Nodes
+#SBATCH -N 4 -n 8 --gres=gpu:2
+#
+#SBATCH --export=ALL
+#
+#SBATCH --job-name="c-met-sams"
+
+build_mpirun_configfile "yank script --yaml=sams-twostage-harmonic-dense.yaml"
+mpiexec.hydra -f hostfile -configfile configfile
